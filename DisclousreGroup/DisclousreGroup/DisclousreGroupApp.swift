94b26ff13e6bbada8656a91e8bca1bd24845f5e4
Ufuk Köşker <48219653+ufukkosker@users.noreply.github.com>
2020-07-14 13:42:37 +0300

    DisclousreGroup
    DisclousreGroup

---
+++ DisclousreGroup/DisclousreGroup/DisclousreGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisclousreGroupApp.swift
+//  DisclousreGroup
+//
+//  Created by Ufuk Köşker on 13.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct DisclousreGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
